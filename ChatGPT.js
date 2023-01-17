73277cc2ceb5866cfc5694d7f9b16934201d94ae
adamsl <eg1972@gmail.com>
2023-01-17 13:45:35 -0500

    it works.

--- ChatGPT.js
+++ ChatGPT.js
@@ -1,20 +1,29 @@
 const { Configuration, OpenAIApi } = require( "openai" );
+const dotenv = require('dotenv');
 
-const configuration = new Configuration( {
-    apiKey: process.env.OPENAI_API_KEY,
-} );
+
+dotenv.config();
+
+//Access environment variables
+const openAiApiKey = process.env.OPENAI_API_KEY;
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
 const openai = new OpenAIApi( configuration );
 
-const response = await openai.createCompletion( {
-    model: "code-davinci-002",
-    prompt: "You: How do I combine arrays?\nJavaScript chatbot: You can use the concat() method.\nYou: How do you make an alert appear after 10 seconds?\nJavaScript chatbot",
-    temperature: 0,
-    max_tokens: 60,
-    top_p: 1.0,
-    frequency_penalty: 0.5,
-    presence_penalty: 0.0,
-    stop: [ "You:" ],
-});
-
-console.log( response.data.choices[0].text );
-
+async function myFunction() {
+    const response = await openai.createCompletion( {
+      model: "code-davinci-002",
+      prompt: "What is the max_tokens key used for when making prompts?",
+      temperature: 0,
+      max_tokens: 600,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+    });
+  
+    console.log( response.data.choices[0].text );
+  }
+  
+  myFunction();
